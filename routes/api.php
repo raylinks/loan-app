5908d967a6b2cad1c6f9d7b7adabda263bbcfaed
Raymond Rilwan <olawuyirilwan98@gmail.com>
2021-07-17 23:37:34 +0100

    cors

--- routes/api.php
+++ routes/api.php
@@ -25,6 +25,8 @@ use App\Http\Controllers\User\InitiateRepaymentController;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+header('Access-Control-Allow-Origin', '*');
+header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
